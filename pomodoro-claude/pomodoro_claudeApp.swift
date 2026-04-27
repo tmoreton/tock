00b6af5f39df57c681294459da399905bdf7fa1e
Tim Moreton <tmoreton89@gmail.com>
2026-04-27 11:38:40 -0400

    Initial Commit

---
+++ pomodoro-claude/pomodoro_claudeApp.swift
@@ -0,0 +1,17 @@
+//
+//  pomodoro_claudeApp.swift
+//  pomodoro-claude
+//
+//  Created by Tim Moreton on 4/27/26.
+//
+
+import SwiftUI
+
+@main
+struct pomodoro_claudeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
